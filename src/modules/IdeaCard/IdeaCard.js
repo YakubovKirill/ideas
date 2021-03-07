2b46e9e930da69a61f1ec8e7d10d0278fbab150b
Yakubov Kirill <yakubov.kirill.work@gmail.com>
2021-03-07 10:42:22 +0300

    feat: remove like btn

--- src/modules/IdeaCard/IdeaCard.js
+++ src/modules/IdeaCard/IdeaCard.js
@@ -15,13 +15,6 @@ function IdeaCard(props) {
                 <div className="idea-header"><p>30 seconds to mars</p></div>
                 <div className="idea-text"> <p> Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text</p></div>
             </div>
-
-            <div className="buttons-block f-c">
-                <div className="like-btn f-c">
-                    <img src="like.svg" alt="like" className="icon-img" />
-                    <p>2k</p>
-                </div>
-            </div>
         </div>
     )
 }
